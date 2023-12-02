e8a0ebe95d12430d8b010151d782257b9d260a31
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2023-12-02 01:02:59 +0100

    :tada: succeeded first part of first day

--- Day1/main.py
+++ Day1/main.py
@@ -1,12 +1,62 @@
 #! python3
 
 import sys
+import re
 
 def displayData(lst):
     print("------#Data#------")
     for i in lst:
         print(i)
     print("------END--------")
+
+def workEachLine(line):
+    strLine = line
+    reverseStr = line [::-1]
+    fisrt = re.findall('[0-9]+',strLine)
+
+    print(fisrt)
+    firstDigit = fisrt[0][0]
+    secondNumber = fisrt[len(fisrt)-1]
+    print("second number : ", end="")
+    print(secondNumber)
+    lastDigit = secondNumber[len(secondNumber)-1]
+    print("lastDigit number : ", end="")
+    print(lastDigit)
+
+    trois = firstDigit + lastDigit
+    trois = int(trois)
+    # if len(fisrt) == 1:
+    #     trois = int(fisrt[0][0])
+    # else: 
+    #     trois = fisrt[0][0] + fisrt[len(fisrt)-1]
+    print(trois)
+    # second = re.findall('[0-9]+',reverseStr)
+    
+    # needSecond = True
+    # hasOtherThanDigitTab = re.findall('\D+', line) 
+    # if len(hasOtherThanDigitTab) == 0:
+    #     needSecond = False
+
+    # # if len(fisrt) == 1:
+    # #     needSecond = False
+
+    # print(fisrt[0] )
+
+    # print(second[0])
+    # second = second[0]
+    # second = second[::-1]
+    # print(second)
+    # if int(fisrt[0] ) == int(second):
+    #     needSecond = False
+    # print("need second : ", end="")
+    # print(needSecond)
+    # if needSecond:
+    #     trois = fisrt[0] + second
+    # else:
+    #     trois = fisrt[0] 
+    # print("trois ", end="")
+    # print (trois)
+    return trois
 
 def readFile(av):
     fd = open(av[1], 'r')
@@ -15,9 +65,21 @@ def readFile(av):
     input = fd.read()
     lst = input.split('\n')
 
-    displayData(lst)
+    #displayData(lst)
     #workOnList(lst)
+    lstTab = []
+    for i in lst:
+        lstTab.append(workEachLine(i))
+    # print("ici")
+    dayOneRes = 0
+    for j in lstTab:
+        #print (j)
+        dayOneRes += int(j)
 
+    #print("res")
+    #print(lstTab)
+    print('le resultat final : ', end='' )
+    print(dayOneRes)
     fd.close()
     
 
